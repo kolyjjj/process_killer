a7845ac5374e07e517e47037156695e306186e14
kolyjjj <me.koly.li@gmail.com>
2013-05-26 19:51:06 +0800

    add test structure

---
+++ Processkiller/src/test/java/li/koly/processkiller/MainActivityTest.java
@@ -0,0 +1,13 @@
+package li.koly.processkiller;
+
+/**
+ * Created by tw on 5/26/13.
+ */
+public class MainActivityTest {
+
+    @Test
+    public void should_call_on_create(){
+        MainActivity mainActivity = new MainActivity();
+    }
+
+}
