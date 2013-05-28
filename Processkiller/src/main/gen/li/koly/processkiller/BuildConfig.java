81d1e4ba23c9bc9ead0be387df4d4008dbc4d367
kolyjjj <me.koly.li@gmail.com>
2013-05-28 16:13:46 +0800

    [koly] track the gen file

---
+++ Processkiller/src/main/gen/li/koly/processkiller/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package li.koly.processkiller;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
