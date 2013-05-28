0310d01cfdb34f6437fc2c69182d372a10ea1d6c
kolyjjj <me.koly.li@gmail.com>
2013-05-28 16:12:07 +0800

    [koly] Move classes out of MainActivity

---
+++ Processkiller/src/main/java/li/koly/processkiller/AppSectionsPagerAdapter.java
@@ -0,0 +1,39 @@
+package li.koly.processkiller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+* Created by tw on 5/28/13.
+*/
+class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+    public AppSectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i) {
+            case 0:
+                return new LaunchpadSectionFragment();
+            default:
+                Fragment fragment = new DummySectionFragment();
+                Bundle args = new Bundle();
+                args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
+                fragment.setArguments(args);
+                return fragment;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Section" + (position + 1);
+    }
+}
---
+++ Processkiller/src/main/java/li/koly/processkiller/LaunchpadSectionFragment.java
@@ -0,0 +1,38 @@
+package li.koly.processkiller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+* Created by tw on 5/28/13.
+*/
+class LaunchpadSectionFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_section_launchpad, container, false);
+
+        rootView.findViewById(R.id.demo_collection_button)
+                .setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getActivity(), CollectionDemoActivity.class);
+                        startActivity(intent);
+                    }
+                });
+        rootView.findViewById(R.id.demo_external_activity)
+                .setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
+                        externalActivityIntent.setType("image/*");
+                        externalActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                        startActivity(externalActivityIntent);
+                    }
+                });
+        return rootView;
+    }
+}
