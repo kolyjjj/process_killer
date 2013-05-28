633a73954a1e7280d011056117ad16814973d19c
yqsqq <yqsqq@163.com>
2013-05-28 23:20:15 +0800

    [Qingsong] Changed the project layout

--- Processkiller/src/main/gen/li/koly/processkiller/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package li.koly.processkiller;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
